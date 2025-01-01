8ad85ecc19ef60ba48b616ab8056402d01686ba4
vpnineh <fffarbod@bk.ru>
2025-01-02 00:05:46 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,8 @@
 <?php
 
 $subscription_urls = [
-  #"https://github.com/vpnineh/sub/raw/refs/heads/main/hub",
-  #"https://github.com/Surfboardv2ray/Proxy-sorter/raw/refs/heads/main/custom/mahsa.txt",
-  #"https://raw.githubusercontent.com/Kwinshadow/TelegramV2rayCollector/main/sublinks/mix.txt",
-  "https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt",
-  #"https://raw.githubusercontent.com/vpnineh/sub/refs/heads/main/go",
-  "https://raw.githubusercontent.com/coldwater-10/V2RayAggregator/refs/heads/master/Eternity.txt",
-  #"https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/main/output/converted.txt",
-  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
-  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/ss_iran.txt",
-  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/ss_iran.txt",
-  #"https://raw.githubusercontent.com/coldwater-10/V2ray-Config-Lite/refs/heads/main/Splitted-By-Protocol/ss.txt",
-
+  "https://raw.githubusercontent.com/NiREvil/vless/refs/heads/main/sub/SSTime",
+  "https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/main/sub/Germany/config.txt",
+  "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/de/mixed",
+  "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/nl/mixed",
 ];
