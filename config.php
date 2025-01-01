ed2f3f26169ace0c314e5c375ec1264d3eca208e
vpnineh <fffarbod@bk.ru>
2025-01-02 00:27:26 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 
 $subscription_urls = [
-  "https://raw.githubusercontent.com/NiREvil/vless/refs/heads/main/sub/SSTime",
+  #"https://raw.githubusercontent.com/NiREvil/vless/refs/heads/main/sub/SSTime",
   "https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/main/sub/Germany/config.txt",
   "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/de/mixed",
   "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/nl/mixed",
-  
+  "https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/refs/heads/main/sub/Netherlands/config.txt",
+
 ];
