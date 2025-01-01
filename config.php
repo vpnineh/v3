8b7c6842a63e5ed5dbc5c654370efb7ebcc051fa
vpnineh <fffarbod@bk.ru>
2025-01-01 23:58:45 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$subscription_urls = [
+  #"https://github.com/vpnineh/sub/raw/refs/heads/main/hub",
+  #"https://github.com/Surfboardv2ray/Proxy-sorter/raw/refs/heads/main/custom/mahsa.txt",
+  #"https://raw.githubusercontent.com/Kwinshadow/TelegramV2rayCollector/main/sublinks/mix.txt",
+  "https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt",
+  #"https://raw.githubusercontent.com/vpnineh/sub/refs/heads/main/go",
+  "https://raw.githubusercontent.com/coldwater-10/V2RayAggregator/refs/heads/master/Eternity.txt",
+  #"https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/main/output/converted.txt",
+  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
+  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/ss_iran.txt",
+  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/ss_iran.txt",
+  #"https://raw.githubusercontent.com/coldwater-10/V2ray-Config-Lite/refs/heads/main/Splitted-By-Protocol/ss.txt",
+
+];
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+include "config.php";
+include "functions.php";
+
+$data = file_get_contents("data.txt");
+$data1 = file_get_contents("https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt");
+
+
+
+if($data != $data1)
+{
+  
+  $merged_data = merge_subscription($subscription_urls);
+  $merged_vmess = array_to_subscription($merged_data['vmess']);
+  $merged_vless = array_to_subscription($merged_data['vless']);
+  $merged_reality = get_reality($merged_vless);
+  $merged_trojan = array_to_subscription($merged_data['trojan']);
+  $merged_shadowsocks = array_to_subscription($merged_data['ss']);
+  $merged_mix = $merged_vmess . $merged_shadowsocks . $merged_vless . $merged_trojan ;
+
+  
+file_put_contents("merged", $merged_mix);
+file_put_contents("merged_base64", base64_encode($merged_mix));
+
+  # Random Lines
+$lines = file('merged');
+shuffle($lines);
+$random_lines = array_slice($lines, 0, 50);
+file_put_contents("lite", $random_lines);
+
+  
+  //Archive - start
+
+    date_default_timezone_set('Iran');
+    $date  = date('Y-m-d H:i:s');
+    $fileName = 'Archive';
+    $fileName = $fileName.'_'.$date;
+    $path = 'update/'.$fileName.'.txt';
+    $path2 = 'update/ss/'.$fileName.'.txt';
+
+file_put_contents($path, $merged_mix);
+file_put_contents($path2, $merged_shadowsocks);
+
+  //Archive - End
+
+file_put_contents("Split/Normal/vmess", $merged_vmess);
+file_put_contents("Split/Base64/vmess", base64_encode($merged_vmess));
+file_put_contents("Split/Normal/vless", $merged_vless);
+file_put_contents("Split/Base64/vless", base64_encode($merged_vless));
+file_put_contents("Split/Normal/reality", $merged_reality);
+file_put_contents("Split/Base64/reality", base64_encode($merged_reality));
+file_put_contents("Split/Normal/trojan", $merged_trojan);
+file_put_contents("Split/Base64/trojan", base64_encode($merged_trojan));
+file_put_contents("Split/Normal/shadowsocks", $merged_shadowsocks);
+file_put_contents("Split/Base64/shadowsocks", base64_encode($merged_shadowsocks));
+file_put_contents("data.txt", file_get_contents("https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt"));
+
+exit(0);
+} else {
+    // If no changes, still exit with success
+    echo "No updates detected. Exiting successfully.\n";
+    exit(0);
+}
