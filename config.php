52688eeac84dfc3794898e36c9515e2939e5f263
vpnineh <fffarbod@bk.ru>
2025-01-02 03:19:43 +0330

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 $subscription_urls = [
   #"https://raw.githubusercontent.com/NiREvil/vless/refs/heads/main/sub/SSTime",
-  "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/de/mixed",
-  "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/nl/mixed",
-  "https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/main/sub/Germany/config.txt",
-  "https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/refs/heads/main/sub/Sweden/config.txt",
-  "https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/refs/heads/main/sub/Netherlands/config.txt",
-
+  #"https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/de/mixed",
+  #"https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/nl/mixed",
+  #"https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/main/sub/Germany/config.txt",
+  #"https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/refs/heads/main/sub/Sweden/config.txt",
+  #"https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/refs/heads/main/sub/Netherlands/config.txt",
+  "https://hiddify-freevpnhomes-subscription.meshkintaj.homes",
 ];
