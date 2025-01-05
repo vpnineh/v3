0eff98b1f91593f83e38df0a91e4dea8bec60930
vpnineh <fffarbod@bk.ru>
2025-01-05 11:15:40 +0330

    Update config.php

--- config.php
+++ config.php
@@ -9,4 +9,5 @@ $subscription_urls = [
   #"https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/refs/heads/main/sub/Netherlands/config.txt",
   #"https://hiddify-freevpnhomes-subscription.meshkintaj.homes",
   "https://github.com/Surfboardv2ray/Subs/raw/refs/heads/main/IPTest/output",
+  "https://raw.githubusercontent.com/ermaozi/get_subscribe/refs/heads/main/subscribe/v2ray.txt",
 ];
