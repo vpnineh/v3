22ae5f007d313f446d9fe3e8eebbc2558af9c9f4
vpnineh <fffarbod@bk.ru>
2025-07-11 18:41:10 +0330

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $subscription_urls = [
   #"https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/refs/heads/main/sub/Sweden/config.txt",
   #"https://raw.githubusercontent.com/Epodonios/bulk-xray-v2ray-vless-vmess-...-configs/refs/heads/main/sub/Netherlands/config.txt",
   #"https://hiddify-freevpnhomes-subscription.meshkintaj.homes",
-  #"https://github.com/Surfboardv2ray/Subs/raw/refs/heads/main/IPTest/output",
+  "https://github.com/Surfboardv2ray/Subs/raw/refs/heads/main/IPTest/output",
   #"https://raw.githubusercontent.com/ermaozi/get_subscribe/refs/heads/main/subscribe/v2ray.txt",
-  "https://raw.githubusercontent.com/itsyebekhe/PSGP/refs/heads/main/private_configs.json",
+  #"https://raw.githubusercontent.com/itsyebekhe/PSGP/refs/heads/main/private_configs.json",
 ];
