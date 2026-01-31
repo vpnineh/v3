8bd94cb4f7ebfbe2416444807abe7cae8c870b49
vpnineh <fffarbod@bk.ru>
2026-02-01 02:06:00 +0330

    Update config.php

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ $subscription_urls = [
   #"https://github.com/Surfboardv2ray/Subs/raw/refs/heads/main/IPTest/output",
   #"https://raw.githubusercontent.com/ermaozi/get_subscribe/refs/heads/main/subscribe/v2ray.txt",
   "https://raw.githubusercontent.com/roosterkid/openproxylist/b01cd5defd19736518c387f279e24f3874136177/V2RAY_RAW.txt",
-  "https://github.com/vpnineh/wow/raw/refs/heads/main/sub",
+  "https://raw.githubusercontent.com/Aa64n/Aa64n-/refs/heads/main/family",
   "https://raw.githubusercontent.com/vpnineh/tlgrm/refs/heads/main/subscription/normal/mix",
     "https://raw.githubusercontent.com/sakha1370/OpenRay/refs/heads/main/output_iran/iran_top100_checked.txt"
 ];
