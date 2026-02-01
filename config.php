932b1b05bc9cf3b8afa709f5acf287f18da9a23d
vpnineh <fffarbod@bk.ru>
2026-02-01 19:07:16 +0330

    Update config.php

--- config.php
+++ config.php
@@ -10,11 +10,11 @@ $subscription_urls = [
   #"https://hiddify-freevpnhomes-subscription.meshkintaj.homes",
   #"https://github.com/Surfboardv2ray/Subs/raw/refs/heads/main/IPTest/output",
   #"https://raw.githubusercontent.com/ermaozi/get_subscribe/refs/heads/main/subscribe/v2ray.txt",
-  "https://raw.githubusercontent.com/roosterkid/openproxylist/b01cd5defd19736518c387f279e24f3874136177/V2RAY_RAW.txt",
-  "https://raw.githubusercontent.com/Aa64n/Aa64n-/refs/heads/main/family",
+  #"https://raw.githubusercontent.com/roosterkid/openproxylist/b01cd5defd19736518c387f279e24f3874136177/V2RAY_RAW.txt",
+  #"https://raw.githubusercontent.com/Aa64n/Aa64n-/refs/heads/main/family",
   "https://raw.githubusercontent.com/vpnineh/tlgrm/refs/heads/main/subscription/normal/mix",
   "https://raw.githubusercontent.com/sakha1370/OpenRay/refs/heads/main/output_iran/iran_top100_checked.txt",
-  "https://github.com/free-nodes/v2rayfree/raw/3118eff2a9abc8793963b88f742232637c6b5b30/README.md",
-  "https://github.com/ALIILAPRO/v2rayNG-Config/raw/afa773fb8ae26fee8f3cbe43ea2304e86ecbefb8/server.txt",
+  #"https://github.com/free-nodes/v2rayfree/raw/3118eff2a9abc8793963b88f742232637c6b5b30/README.md",
+  #"https://github.com/ALIILAPRO/v2rayNG-Config/raw/afa773fb8ae26fee8f3cbe43ea2304e86ecbefb8/server.txt",
   "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/row-url/actives.txt"
 ];
